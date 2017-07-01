1135a1e39c9b8310aaab1e6b9d105a6215b8c422
Panagiotis Bakos <panbac88@hotmail.com>
2017-07-01 18:32:38 +0300

    no message

--- Elias.Web/Dialogs/LUISDialog.cs
+++ Elias.Web/Dialogs/LUISDialog.cs
@@ -5,11 +5,13 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using static Microsoft.Bot.Builder.Luis.BuiltIn.DateTime;
 
 namespace Elias.Web.Dialogs
 {
@@ -53,7 +55,25 @@ namespace Elias.Web.Dialogs
         [LuisIntent("RequestLeave")]
         public async Task RequestLeave(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"request leave");
+            if (result.Entities.Count != 0 && result.Entities.Count < 3)
+            {
+                if (result.Entities.First().Type == "builtin.datetimeV2.daterange")
+                {
+
+                    var daterange = result.Entities.First().Resolution["values"];
+                    //var startDate = daterange["start"].ToObject<DateTime>();
+                    //var endDate = daterange["end"].ToObject<DateTime>();
+
+                }
+                else if (result.Entities.Count == 2 && result.Entities.All(e => e.Type == "builtin.datetimeV2.date"))
+                {
+
+                }
+            }
+            else
+            {
+                await context.PostAsync($"I'm not sure about when you want your leave. Maybe try something like \"I'd like a few days off between dd/mm/yy and dd/mm/yy\"");
+            }
         }
 
         [LuisIntent("RequestRemaining")]
